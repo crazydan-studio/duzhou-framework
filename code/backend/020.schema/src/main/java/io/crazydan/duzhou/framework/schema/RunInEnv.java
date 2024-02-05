512440076f78f5ea0e8595680e835df96260077e
flytreeleft <flytreeleft@crazydan.org>
2024-02-05 22:42:10 +0800

    Backend/schema: 启用在 Maven 构建时自动从 xdef 生成 class 代码

---
+++ code/backend/020.schema/src/main/java/io/crazydan/duzhou/framework/schema/RunInEnv.java
@@ -0,0 +1,40 @@
+/*
+ * 渡舟平台 - 致力于构建自运维、自监控、可演化的全功能型应用平台
+ * Copyright (C) 2024 Crazydan Studio <https://studio.crazydan.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html#license-text>.
+ */
+
+package io.crazydan.duzhou.framework.schema;
+
+/**
+ * 服务运行环境
+ * <p/>
+ * 服务运行的环境类型标识，不同环境下的服务会有不同的配置数据
+ *
+ * @author <a href="mailto:flytreeleft@crazydan.org">flytreeleft</a>
+ * @date 2024-02-05
+ */
+public enum RunInEnv {
+    /** 开发环境 */
+    development,
+    /** 阶段构建环境 */
+    staging,
+    /** 测试环境 */
+    testing,
+    /** 生产环境 */
+    production,
+    ;
+}
---
+++ code/backend/020.schema/src/test/java/io/crazydan/duzhou/framework/schema/SchemaCodeGen.java
@@ -0,0 +1,29 @@
+package io.crazydan.duzhou.framework.schema;
+
+import java.io.File;
+
+import io.nop.api.core.config.AppConfig;
+import io.nop.codegen.XCodeGenerator;
+import io.nop.commons.util.MavenDirHelper;
+import io.nop.core.CoreConfigs;
+import io.nop.core.CoreConstants;
+import io.nop.core.initialize.CoreInitialization;
+
+/** 用于调试从 *.xgen 生成代码过程中的问题 */
+public class SchemaCodeGen {
+
+    public static void main(String[] args) {
+        AppConfig.getConfigProvider()
+                 .updateConfigValue(CoreConfigs.CFG_CORE_MAX_INITIALIZE_LEVEL,
+                                    CoreConstants.INITIALIZER_PRIORITY_ANALYZE);
+
+        CoreInitialization.initialize();
+        try {
+            File projectDir = MavenDirHelper.projectDir(SchemaCodeGen.class);
+
+            XCodeGenerator.runPrecompile(projectDir, "/", false);
+        } finally {
+            CoreInitialization.destroy();
+        }
+    }
+}
