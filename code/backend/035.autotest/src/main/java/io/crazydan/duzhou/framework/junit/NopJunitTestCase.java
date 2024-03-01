cbea1554d2395020069d34e47892aa3fd45b215a
flytreeleft <flytreeleft@crazydan.org>
2024-03-01 11:38:29 +0800

    Backend: 新增 autotest 模块

---
+++ code/backend/035.autotest/src/main/java/io/crazydan/duzhou/framework/junit/NopJunitTestCase.java
@@ -0,0 +1,44 @@
+/*
+ * 渡舟平台 - 致力于构建自运维、自监控、可演化的全功能型应用平台
+ * Copyright (C) 2024 Crazydan Studio <https://studio.crazydan.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html#license-text>.
+ */
+
+package io.crazydan.duzhou.framework.junit;
+
+import java.io.File;
+
+import io.nop.autotest.junit.JunitBaseTestCase;
+import io.nop.commons.util.FileHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="mailto:flytreeleft@crazydan.org">flytreeleft</a>
+ * @date 2024-03-01
+ */
+public abstract class NopJunitTestCase extends JunitBaseTestCase {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    public NopJunitTestCase() {
+        // 统一设置测试样例数据位置为当前用例目录下
+        try {
+            File dir = FileHelper.getClassPathFile("cases/" + getClass().getSimpleName());
+            setAttachmentDir(dir);
+        } catch (Exception ignore) {
+        }
+    }
+}
