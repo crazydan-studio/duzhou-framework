b86f9a666f93d0d30cf16e27eb28b7891aff00b0
flytreeleft <flytreeleft@crazydan.org>
2024-03-12 23:11:58 +0800

    Frontend/web: 修复 DslEditor 展开节点树时出现闪跳的问题

--- code/frontend/web/src/renderer/amis/components/diagram/DslEditor.tsx
+++ code/frontend/web/src/renderer/amis/components/diagram/DslEditor.tsx
@@ -120,6 +120,7 @@ function ReactFlowEditor() {
       const { parent, create } = node.data;
       const newNode = create();
       newNode.id = uuid();
+      newNode.parent = parent;
       newNode.position = { ...node.position };
 
       const newEdge = {
@@ -164,23 +165,34 @@ function ReactFlowEditor() {
           const n = getNode(id);
           targets[id] = {
             ...n,
-            hidden: false,
-            _hidden: collapsed,
-            // 子孙节点的展开状态与当前节点的一致
-            _collapsed: collapsed
+            // 已隐藏的，先保持隐藏状态，
+            // 以避免动画移动出现闪跳（移动的初始位置需要在重新布局后才能确定）
+            // hidden: n.hidden,
+            _hidden: collapsed
           };
 
           const subIds = links[id];
           delete links[id];
 
-          travel(subIds);
+          // 忽略已收缩的树，确保其收缩状态保持不变
+          if (!n._collapsed) {
+            travel(subIds);
+          }
         });
       };
       travel(links[node.id]);
 
       setEdges(
         edges.map((e) =>
-          targets[e.target] ? { ...e, hidden: false, _hidden: collapsed } : e
+          targets[e.target]
+            ? {
+                ...e,
+                // 保持展开树中的连线隐藏状态，仅在开始发生位置变化时才显示，
+                // 以避免目标节点位置未确定而出现连线闪跳
+                _hidden: collapsed,
+                _collapsed: true
+              }
+            : e
         )
       );
 
@@ -233,43 +245,49 @@ const useAutoLayout = () => {
     .nodeSize((node) => [node.data.height * 1.5, node.data.width * 1.5])
     .spacing(() => 1);
 
-  function layoutNodes(nodes, edges) {
-    const parents = {};
-    edges.forEach((e) => {
-      parents[e.target] = e.source;
-    });
-
-    let collapsed = { id: '', position: {} };
-    nodes.forEach((n) => {
-      n.selected && (collapsed = n);
-    });
-
+  function layoutNodes(nodes, isInCollapseTree) {
     // stratify() 用于将扁平数据转换为树形结构
     const tree = stratify()
       .id((d) => d.id)
-      .parentId((d) => parents[d.id])(nodes.filter((n) => !n._hidden));
+      .parentId((d) => d.parent)(
+      // 仅对不需要隐藏的节点进行布局
+      nodes.filter((n) => !n._hidden)
+    );
 
-    const map = {};
-    const root = layout(tree);
-    root.descendants().forEach((d) => {
-      map[d.id] = d;
-    });
+    const positions = {};
+    layout(tree)
+      .descendants()
+      .forEach((d) => {
+        // 交换 tree 布局的 x/y 坐标，以将垂直方向布局转换为水平方向布局
+        positions[d.id] = { x: d.y, y: d.x };
+      });
 
-    return nodes.map((node) => {
-      const d = map[node.id];
-      const pd = map[collapsed.id];
-      return {
-        ...node,
-        position: d
-          ? {
-              // x，y 交换位置后，便变为水平布局
-              x: d.y,
-              y: d.x
-            }
-          : // 待隐藏节点向父节点靠拢
-            { x: pd.y, y: pd.x }
-      };
-    });
+    const collapseRoot = nodes.find((n) => n.selected);
+    const collapseRootPos = positions[collapseRoot?.id];
+    let isCollapseNode = false;
+
+    return nodes.map(
+      (node) => (
+        (isCollapseNode = isInCollapseTree(node, collapseRoot?.id)),
+        {
+          id: node.id,
+          node: {
+            ...node,
+            // 若不在展开树中，则保持隐藏状态，否则，始终不隐藏
+            hidden: node.hidden && !isCollapseNode
+          },
+          // 若节点需显示且其在展开书中，则从展开树的根节点的位置移动节点
+          from:
+            !node._hidden && isCollapseNode
+              ? collapseRoot.position
+              : node.position,
+          // 需显示的节点采用布局后的坐标，待隐藏的节点采用待收缩树的根节点坐标，余下节点的坐标不变
+          to:
+            positions[node.id] ||
+            (node._hidden ? collapseRootPos : node.position)
+        }
+      )
+    );
   }
 
   function Layout(options) {
@@ -299,14 +317,21 @@ const useAutoLayout = () => {
       const nodes = getNodes();
       const edges = getEdges();
 
-      const transitions = layoutNodes(nodes, edges).map((node) => {
-        return {
-          id: node.id,
-          from: getNode(node.id)?.position || node.position,
-          to: node.position,
-          node
-        };
-      });
+      // 检查节点是否在展开树中
+      const isInCollapseTree = (node, rootId, level = 0) =>
+        rootId && node && !(level > 0 && node._collapsed) && node.parent
+          ? node.parent === rootId ||
+            isInCollapseTree(getNode(node.parent), rootId, level + 1)
+          : false;
+
+      const transitions = layoutNodes(nodes, isInCollapseTree);
+      // 为待移动节点设置初始位置
+      setNodes(
+        transitions.map(({ node, from }) => ({
+          ...node,
+          position: from
+        }))
+      );
 
       const t = timer((elapsed) => {
         if (elapsed < options.duration) {
@@ -321,6 +346,10 @@ const useAutoLayout = () => {
               }
             }))
           );
+
+          setEdges(
+            edges.map((e) => (e._collapsed ? { ...e, hidden: false } : e))
+          );
         } else {
           t.stop();
 
@@ -332,7 +361,14 @@ const useAutoLayout = () => {
               position: { ...to }
             }))
           );
-          setEdges(edges.map((e) => ({ ...e, hidden: e._hidden })));
+
+          setEdges(
+            edges.map((e) => ({
+              ...e,
+              hidden: e._hidden,
+              _collapsed: false
+            }))
+          );
 
           if (!initial.current) {
             initial.current = true;
@@ -548,3 +584,8 @@ const initialEdges = [
     ...newNodeEdgeProps
   }
 ]);
+
+initialNodes.forEach((node) => {
+  const edge = initialEdges.find((edge) => edge.target === node.id);
+  edge && (node.parent = edge.source);
+});
