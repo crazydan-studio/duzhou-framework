fd1a132f7d909214d8bd35ab4f752c10f192f39e
flytreeleft <flytreeleft@crazydan.org>
2024-04-20 22:41:24 +0800

    Backend: 移除 app-dev 模块，其将被迁移至 platform 项目中

--- code/backend/800.app-dev/biz/src/main/java/io/crazydan/duzhou/framework/app_dev/biz/AppDevBizConstants.java
+++
@@ -1,6 +0,0 @@
-package io.crazydan.duzhou.framework.app_dev.biz;
-
-/** 业务常量定义 */
-public interface AppDevBizConstants {
-
-}
--- code/backend/800.app-dev/orm/src/main/java/io/crazydan/duzhou/framework/app_dev/orm/AppDevOrmConstants.java
+++
@@ -1,6 +0,0 @@
-package io.crazydan.duzhou.framework.app_dev.orm;
-
-/** ORM 字典常量定义 */
-public interface AppDevOrmConstants extends _AppDevOrmConstants {
-
-}
