8209c1150911ee23ee58ab73a8fc1a6d47c71a9c
flytreeleft <flytreeleft@crazydan.org>
2024-04-22 23:01:56 +0800

    Backend/autotest: 新增 NopJunitAutoTestCase 用于调整测试的样例数据存放位置

---
+++ code/backend/035.autotest/src/main/java/io/crazydan/duzhou/framework/junit/NopJunitAutoTestCase.java
@@ -0,0 +1,49 @@
+/*
+ * 渡舟平台 - 致力于构建自运维、自监控、可演化的全功能型应用平台
+ * Copyright (C) 2024 Crazydan Studio <https://studio.crazydan.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html#license-text>.
+ */
+
+package io.crazydan.duzhou.framework.junit;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
+import io.nop.autotest.junit.JunitAutoTestCase;
+import org.junit.jupiter.api.TestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="mailto:flytreeleft@crazydan.org">flytreeleft</a>
+ * @date 2024-04-22
+ */
+public abstract class NopJunitAutoTestCase extends JunitAutoTestCase {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public File getCasesDir() {
+        return new File(getTestResourcesDir(), "auto-cases");
+    }
+
+    @Override
+    protected String getCaseDataPath(TestInfo testInfo) {
+        Class<?> cls = testInfo.getTestClass().orElse(null);
+        Method method = testInfo.getTestMethod().orElse(null);
+
+        return cls.getSimpleName() + "/" + method.getName();
+    }
+}
