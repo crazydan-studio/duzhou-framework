4754b5f7dabe8578bc383138110ac0865d32765b
flytreeleft <flytreeleft@crazydan.org>
2025-02-07 19:42:46 +0800

    去掉 client 子项目，本框架全部针对后端开发，与客户端相关的处理服务放在后端的 ui 子模块中，以便于从底层实现对 UI 的差量化支持

--- code/client/web/public/pages/signin/config.js
+++
@@ -1,6 +0,0 @@
-window.__APP_SITE_CONFIG__ = {
-  container: '#app',
-  data: {},
-  // schemaApi: '@query:PageProvider__getPage?path=/duzhou/web/pages/auth/signin.page.xml'
-  schemaApi: '/pages/signin/page.json'
-};
