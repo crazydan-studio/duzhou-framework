5401c230cf340dc284fc6a1c83b0bbafa3c29f05
flytreeleft <flytreeleft@crazydan.org>
2025-04-07 11:28:12 +0800

    UI Schema: 明确组件内事件和消息的触发和监听机制，即，组件内可在消息处理器中向外发送事件，而在父组件中则可监听子组件的事件以派发对应的消息到本组件的消息处理器中

--- code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/XuiComponentEvent.java
+++
@@ -1,9 +0,0 @@
-package io.crazydan.duzhou.framework.ui.schema.component;
-
-import io.crazydan.duzhou.framework.ui.schema.component._gen._XuiComponentEvent;
-
-public class XuiComponentEvent extends _XuiComponentEvent{
-    public XuiComponentEvent(){
-
-    }
-}
---
+++ code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/XuiComponentMessageDispatch.java
@@ -0,0 +1,9 @@
+package io.crazydan.duzhou.framework.ui.schema.component;
+
+import io.crazydan.duzhou.framework.ui.schema.component._gen._XuiComponentMessageDispatch;
+
+public class XuiComponentMessageDispatch extends _XuiComponentMessageDispatch{
+    public XuiComponentMessageDispatch(){
+
+    }
+}
