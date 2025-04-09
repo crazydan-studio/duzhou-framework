de30b5d9fb6499c0aa3d0ef975e8606566e6d376
flytreeleft <flytreeleft@crazydan.org>
2025-04-09 14:16:49 +0800

    UI Runtime: 完善 Svelte 运行产物构建模板

--- code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/app/XuiAppPageRef.java
+++ code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/app/XuiAppPageRef.java
@@ -1,9 +1,20 @@
 package io.crazydan.duzhou.framework.ui.schema.app;
 
+import io.crazydan.duzhou.framework.ui.schema.XuiPage;
 import io.crazydan.duzhou.framework.ui.schema.app._gen._XuiAppPageRef;
+import io.nop.xlang.xdsl.DslModelHelper;
 
-public class XuiAppPageRef extends _XuiAppPageRef{
-    public XuiAppPageRef(){
+public class XuiAppPageRef extends _XuiAppPageRef {
+    private XuiPage page;
 
+    public XuiAppPageRef() {
+
+    }
+
+    public XuiPage getPage() {
+        if (this.page == null) {
+            this.page = (XuiPage) DslModelHelper.loadDslModelFromPath(getPath());
+        }
+        return this.page;
     }
 }
