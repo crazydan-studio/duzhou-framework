770418b02548ce0eebc8a2a25014a4a2aa98134c
flytreeleft <flytreeleft@crazydan.org>
2025-04-11 23:39:49 +0800

    UI Runtime: 初步实现对原生组件的构建支持

--- code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/XuiComponent.java
+++ code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/XuiComponent.java
@@ -1,12 +1,14 @@
 package io.crazydan.duzhou.framework.ui.schema;
 
 import io.crazydan.duzhou.framework.ui.schema._gen._XuiComponent;
+import io.crazydan.duzhou.framework.ui.schema.component.XuiComponentImport;
+import io.crazydan.duzhou.framework.ui.schema.component.XuiComponentNamed;
 import io.nop.api.core.util.INeedInit;
 
 public class XuiComponent extends _XuiComponent implements INeedInit {
+    public static final String NATIVE_IMPORT_PREFIX = "native:";
 
     public XuiComponent() {
-
     }
 
     @Override
@@ -14,9 +16,18 @@ public class XuiComponent extends _XuiComponent implements INeedInit {
         initTemplate();
     }
 
-    private void initTemplate() {
-        getTemplate().getChildren();
+    /** 如果导入的节点为原生组件，则返回其原生组件类型 */
+    public String getNodeNativeType(XuiComponentNamed node) {
+        String nodeType = node.get$type();
+        XuiComponentImport imported = getImport(nodeType);
 
+        return imported != null && imported.getFrom().startsWith(NATIVE_IMPORT_PREFIX)
+               ? imported.getFrom()
+                         .substring(NATIVE_IMPORT_PREFIX.length())
+               : null;
+    }
+
+    private void initTemplate() {
         // TODO XuiComponentNode 类型的节点的 $type 需满足组件名规范，参考 CheckStdDomainHandler.parseProp 抛出异常
     }
 }
