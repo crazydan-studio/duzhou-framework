ee75bf5d670cac4da69b9a3c14b2f8d3b3f5022c
flytreeleft <flytreeleft@crazydan.org>
2025-04-25 10:07:12 +0800

    UI Schema: 完善 linear 布局的文本标记规范

--- code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/_gen/_XuiComponentLayoutLinear.java
+++ code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/_gen/_XuiComponentLayoutLinear.java
@@ -11,7 +11,7 @@ import io.nop.commons.util.ClassHelper;
 /**
  * generate from /duzhou/ui/schema/component/template.xdef <p>
  * 使用文本标记指示布局
- * - [comp]: 指代 name 为 comp 的组件；
+ * - [comp]: 指代 xui:name 为 comp 的组件；
  * - <: 表示左对齐；
  * - >: 表示右对齐；
  * - ^: 表示顶部对齐；
@@ -21,9 +21,12 @@ import io.nop.commons.util.ClassHelper;
  * - ^[comp]v: 表示使组件 comp 垂直占满剩余空间；
  * - v[comp]^: 表示使组件 comp 垂直居中对齐；
  * - [comp1] [comp2]: 表示左右放置组件 comp1 和 comp2；
- * - [comp1] | [comp2]: 表示左右放置组件 comp1 和 comp2；
+ * - [comp1] | [comp2]: 表示以表格形式放置组件 comp1 和 comp2，也即，comp1 与 comp2 分别在同一行的两个单元格内。
+ * 上下相邻的包含 | 的多行属于同一个表格；
  * - [comp1]\n[comp2]: 表示上下放置组件 comp1 和 comp2，其中，\n 为换行符，实际不可见，效果为上下两行；
- * - {xxx}: 表示布局嵌套，可在内部反复应用前面的规则，或者将其视为一个整体，可对其应用水平和垂直布局；
+ * - {xxx}: 表示布局嵌套，可在内部反复应用前面的规则，其自身也是一个整体，可像组件一样对其应用水平和垂直布局；
+ * - (): 表示设置组件布局配置参数列表，在括号内以 prop:value 形式指定参数名和参数值，且参数间以逗号分隔，
+ * 如：(width:10em,height:20em) 表示设置宽为 10em，长为 20em。参数列表只能紧跟在 [comp] 和 {xxx} 之后放置；
  * - 组件尺寸（宽高）默认均为自适应内容；
  */
 @SuppressWarnings({"PMD.UselessOverridingMethod","PMD.UnusedLocalVariable",
