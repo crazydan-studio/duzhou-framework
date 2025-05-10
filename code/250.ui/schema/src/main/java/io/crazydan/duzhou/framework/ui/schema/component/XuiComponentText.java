3d866aced3dff017d3a0f455fd22e9679ea96ccb
flytreeleft <flytreeleft@crazydan.org>
2025-05-10 18:05:44 +0800

    UI Schema: 处理 XSS 攻击

--- code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/XuiComponentText.java
+++ code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/XuiComponentText.java
@@ -1,9 +1,24 @@
 package io.crazydan.duzhou.framework.ui.schema.component;
 
+import io.crazydan.duzhou.framework.commons.StringHelper;
 import io.crazydan.duzhou.framework.ui.schema.component._gen._XuiComponentText;
 
-public class XuiComponentText extends _XuiComponentText{
-    public XuiComponentText(){
+public class XuiComponentText extends _XuiComponentText {
 
+    public XuiComponentText() {
+    }
+
+    @Override
+    public String getValue() {
+        String value = super.getValue();
+
+        if (value != null) {
+            value = value.trim();
+            // 转义 HTML 以避免 XSS 攻击
+            value = StringHelper.escapeHtml(value);
+            // 恢复空格等转义符号
+            value = value.replaceAll("&amp;nbsp;", "&nbsp;");
+        }
+        return value;
     }
 }
