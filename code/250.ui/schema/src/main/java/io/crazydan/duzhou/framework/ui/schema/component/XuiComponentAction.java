2459aa3352a9110d4cac2ad5998e05e4c1a2b77a
flytreeleft <flytreeleft@crazydan.org>
2025-06-02 17:41:01 +0800

    UI Schema: 删除无用 class

--- code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/XuiComponentAction.java
+++
@@ -1,9 +0,0 @@
-package io.crazydan.duzhou.framework.ui.schema.component;
-
-import io.crazydan.duzhou.framework.ui.schema.component._gen._XuiComponentAction;
-
-public class XuiComponentAction extends _XuiComponentAction{
-    public XuiComponentAction(){
-
-    }
-}
