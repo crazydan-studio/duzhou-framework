9fcaa4fc756e917eee6942eaf9baebb66dc02bc5
flytreeleft <flytreeleft@crazydan.org>
2025-06-05 15:53:36 +0800

    UI Schema: XuiComponentProp#metaPath -> XuiComponentProp#objMeta

--- code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/_gen/_XuiComponentProp.java
+++ code/250.ui/schema/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/_gen/_XuiComponentProp.java
@@ -12,8 +12,15 @@ import io.nop.commons.util.ClassHelper;
  * generate from /duzhou/ui/schema/component/props.xdef <p>
  * 属性定义
  * - 对于 string、bool、int 等基础类型的属性，其不包含结构，故而，仅需配置 `type` 即可；
- * - 对于结构化的属性，可以在子节点中分解其结构，也可以通过 `meta-path` 指定对应的
+ * - 对于结构化的属性，可以在子节点中分解其结构，也可以通过 `obj-meta` 指定对应的
  * xmeta 文件位置，并从中获取其完整的模型结构；
+ * - 解析规则
+ * - 有子结构的，不可配置 `type` 和 `obj-meta`；
+ * - 没有子结构的，`type` 和 `obj-meta` 必须且只能配置一个；
+ * - `defaultValue` 可以为对象，如 `${{a: 1, b: 2}}`，用于为结构属性赋缺省值，
+ * 并且，在其子结构属性上设置的缺省值将与该缺省值做覆盖合并；
+ * - `obj-meta` 对应的 xmeta 将在 `x:gen-extends` 中加载，
+ * 并根据其结构构造当前属性的子结构；
  */
 @SuppressWarnings({"PMD.UselessOverridingMethod","PMD.UnusedLocalVariable",
     "PMD.UnnecessaryFullyQualifiedName","PMD.EmptyControlStatement","java:S116","java:S101","java:S1128","java:S1161"})
@@ -36,10 +43,10 @@ public abstract class _XuiComponentProp extends io.nop.core.resource.component.A
     
     /**
      *  可选
-     * xml name: meta-path
+     * xml name: obj-meta
      * 属性结构的 xmeta 定义文件的 vpath 路径，仅针对可复用的、结构化的属性；
      */
-    private java.lang.String _metaPath ;
+    private java.lang.String _objMeta ;
     
     /**
      *  可选
@@ -101,19 +108,19 @@ public abstract class _XuiComponentProp extends io.nop.core.resource.component.A
     
     /**
      * 可选
-     * xml name: meta-path
+     * xml name: obj-meta
      *  属性结构的 xmeta 定义文件的 vpath 路径，仅针对可复用的、结构化的属性；
      */
     
-    public java.lang.String getMetaPath(){
-      return _metaPath;
+    public java.lang.String getObjMeta(){
+      return _objMeta;
     }
 
     
-    public void setMetaPath(java.lang.String value){
+    public void setObjMeta(java.lang.String value){
         checkAllowChange();
         
-        this._metaPath = value;
+        this._objMeta = value;
            
     }
 
@@ -156,7 +163,7 @@ public abstract class _XuiComponentProp extends io.nop.core.resource.component.A
         
         out.putNotNull("children",this.getChildren());
         out.putNotNull("defaultValue",this.getDefaultValue());
-        out.putNotNull("metaPath",this.getMetaPath());
+        out.putNotNull("objMeta",this.getObjMeta());
         out.putNotNull("type",this.getType());
     }
 
@@ -171,7 +178,7 @@ public abstract class _XuiComponentProp extends io.nop.core.resource.component.A
         
         instance.setChildren(this.getChildren());
         instance.setDefaultValue(this.getDefaultValue());
-        instance.setMetaPath(this.getMetaPath());
+        instance.setObjMeta(this.getObjMeta());
         instance.setType(this.getType());
     }
 
