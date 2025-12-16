a9613178456c5245d656cf5af551f212d12765c0
flytreeleft <flytreeleft@crazydan.org>
2025-12-16 11:37:21 +0800

    UI Schema: 将样式明确分解为定义和引用两类，并在组件/布局中复用其结构

--- code/250.ui/schema/core/src/main/java/io/crazydan/duzhou/framework/ui/XuiConstants.java
+++ code/250.ui/schema/core/src/main/java/io/crazydan/duzhou/framework/ui/XuiConstants.java
@@ -35,7 +35,13 @@ public interface XuiConstants {
     String XDSL_SCHEMA_COMPONENT = "/duzhou/ui/schema/component.xdef";
     String XDSL_SCHEMA_COMPONENT_TEMPLATE = "/duzhou/ui/schema/component/template.xdef";
     String XDSL_SCHEMA_COMPONENT_IMPORT = "/duzhou/ui/schema/component/import.xdef";
+    String XDSL_SCHEMA_COMPONENT_STYLES = "/duzhou/ui/schema/component/styles.xdef";
     String XDSL_SCHEMA_COMPONENT_MESSAGE = "/duzhou/ui/schema/component/message.xdef";
+    String XDSL_SCHEMA_STYLES = "/duzhou/ui/schema/styles.xdef";
+    String XDSL_SCHEMA_STYLE_DEF = "/duzhou/ui/schema/style/style-def.xdef";
+    String XDSL_SCHEMA_STYLE_DEFS = "/duzhou/ui/schema/style/style-defs.xdef";
+    String XDSL_SCHEMA_STYLE_REF = "/duzhou/ui/schema/style/style-ref.xdef";
+    String XDSL_SCHEMA_STYLE_REFS = "/duzhou/ui/schema/style/style-refs.xdef";
 
     String TAG_NAME_TEMPLATE = "template";
     String TAG_NAME_IF = "if";
