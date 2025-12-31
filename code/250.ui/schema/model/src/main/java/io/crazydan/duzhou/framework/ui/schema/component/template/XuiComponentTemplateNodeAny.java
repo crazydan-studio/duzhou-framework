2aecc694bfeee46282e54fa56a78e971ba76ccf8
flytreeleft <flytreeleft@crazydan.org>
2025-12-31 11:30:29 +0800

    UI Schema: 补充 action 结构

--- code/250.ui/schema/model/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/template/XuiComponentTemplateNodeAny.java
+++ code/250.ui/schema/model/src/main/java/io/crazydan/duzhou/framework/ui/schema/component/template/XuiComponentTemplateNodeAny.java
@@ -1,9 +1,18 @@
 package io.crazydan.duzhou.framework.ui.schema.component.template;
 
+import io.crazydan.duzhou.framework.ui.schema.component.XuiComponent;
 import io.crazydan.duzhou.framework.ui.schema.component.template._gen._XuiComponentTemplateNodeAny;
+import io.nop.api.core.validate.IValidationErrorCollector;
 
-public class XuiComponentTemplateNodeAny extends _XuiComponentTemplateNodeAny{
-    public XuiComponentTemplateNodeAny(){
+public class XuiComponentTemplateNodeAny extends _XuiComponentTemplateNodeAny {
 
+    public XuiComponentTemplateNodeAny() {
+    }
+
+    @Override
+    public void validate(XuiComponent component, IValidationErrorCollector collector) {
+        // TODO 在组件上设置的属性必须在组件定义中定义
+
+        super.validate(component, collector);
     }
 }
