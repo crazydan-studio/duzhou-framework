481e388663cfb4d41e6b198592a29a8678bb5460
flytreeleft <flytreeleft@crazydan.org>
2026-02-08 20:52:01 +0800

    Commons: 消除可变参数方法

--- code/030.commons/src/main/java/io/crazydan/duzhou/framework/commons/PnpmRunner.java
+++ code/030.commons/src/main/java/io/crazydan/duzhou/framework/commons/PnpmRunner.java
@@ -48,13 +48,25 @@ public class PnpmRunner {
         this.distDir = distDir;
     }
 
+    // Note: 避免定义带可变参数的重载方法，确保其在 xpl 脚本中可被正确调用
+
+    /** @see #runScript(String, String[]) */
+    public PnpmRunner runScript(String scriptName) {
+        return runScript(scriptName, new String[0]);
+    }
+
     /** 运行 {@code package.json} 中定义的脚本，不遍历执行 workspace 中的脚本 */
-    public PnpmRunner runScript(String scriptName, String... args) {
+    public PnpmRunner runScript(String scriptName, String[] args) {
         return runScript(scriptName, false, args);
     }
 
+    /** @see #runScript(String, boolean, String[]) */
+    public PnpmRunner runScript(String scriptName, boolean recursive) {
+        return runScript(scriptName, recursive, new String[0]);
+    }
+
     /** 运行 {@code package.json} 中定义的脚本 */
-    public PnpmRunner runScript(String scriptName, boolean recursive, String... args) {
+    public PnpmRunner runScript(String scriptName, boolean recursive, String[] args) {
         if (!this.disableInstall) {
             execPnpm("install");
         }
